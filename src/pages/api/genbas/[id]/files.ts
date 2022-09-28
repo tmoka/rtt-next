19442557389208d8efda5a385feb0fc11c248dd4
tmoka <knowledzr+alt@gmail.com>
2022-09-28 15:45:11 +0900

    #16 複数ファイルのアップロード処理を追加

--- src/pages/api/genbas/[id]/files.ts
+++ src/pages/api/genbas/[id]/files.ts
@@ -1,35 +1,39 @@
-import { NextApiRequest, NextApiResponse } from "next"
-import formidable from 'formidable'
+import { NextApiRequest, NextApiResponse } from 'next'
+import formidable, { Formidable } from 'formidable'
 import fs from 'fs'
 
 export const config = {
   api: {
     bodyParser: false,
   },
-};
+}
 
+const form = formidable({ multiples: true })
 
 const filesHandler = (req: NextApiRequest, res: NextApiResponse) => {
   if (req.method === 'POST') {
-    console.log("TEST TEXT")
-    const form = new formidable.IncomingForm();
+    const contentType = req.headers['content-type']
+
     form.parse(req, async function (err, fields, files) {
-      console.log(JSON.stringify(files.file))
-      await saveFile(files.file);
-      return res.status(200).send("");
-    });
-    
-    
+      const acceptedFiles = files.file
+      if (Array.isArray(acceptedFiles)) {
+        acceptedFiles.map(async (elem: any) => {
+          console.log(JSON.stringify(elem))
+          await saveFile(elem)
+        })
+      }
+      return res.status(200).send('')
+    })
     return res.json({})
   }
   return res.json({})
 }
 
 const saveFile = async (file: any) => {
-  const data = fs.readFileSync(file.filepath);
-  fs.writeFileSync(`./public/${file.originalFilename}`, data);
-  await fs.unlinkSync(file.filepath);
-  return;
-};
+  const data = fs.readFileSync(file.filepath)
+  fs.writeFileSync(`./public/${file.originalFilename}`, data)
+  await fs.unlinkSync(file.filepath)
+  return
+}
 
 export default filesHandler

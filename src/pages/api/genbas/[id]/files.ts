ecc0a147dc19c951fca2693c30d164147de68397
tmoka <knowledzr+alt@gmail.com>
2022-09-28 20:45:46 +0900

    #16 アップロード用ディレクトリを指定

--- src/pages/api/genbas/[id]/files.ts
+++ src/pages/api/genbas/[id]/files.ts
@@ -1,6 +1,7 @@
 import { NextApiRequest, NextApiResponse } from 'next'
 import formidable, { Formidable } from 'formidable'
 import fs from 'fs'
+import path from 'path'
 
 export const config = {
   api: {
@@ -11,6 +12,7 @@ export const config = {
 const form = formidable({ multiples: true })
 
 const filesHandler = (req: NextApiRequest, res: NextApiResponse) => {
+  const { id } = req.query
   if (req.method === 'POST') {
     const contentType = req.headers['content-type']
 
@@ -19,19 +21,19 @@ const filesHandler = (req: NextApiRequest, res: NextApiResponse) => {
       if (Array.isArray(acceptedFiles)) {
         acceptedFiles.map(async (elem: any) => {
           console.log(JSON.stringify(elem))
-          await saveFile(elem)
+          await saveFile(elem, String(id))
         })
       }
       return res.status(200).send('')
     })
-    return res.json({})
   }
-  return res.json({})
 }
 
-const saveFile = async (file: any) => {
+const saveFile = async (file: any, genbaId: string) => {
   const data = fs.readFileSync(file.filepath)
-  fs.writeFileSync(`./public/${file.originalFilename}`, data)
+  const uploadPath = path.join('uploads', 'genbas', genbaId)
+  fs.existsSync(uploadPath) ? null : fs.mkdirSync(uploadPath)
+  fs.writeFileSync(uploadPath + `/${file.originalFilename}`, data)
   await fs.unlinkSync(file.filepath)
   return
 }
--- src/pages/rttweb/[id]/files.tsx
+++ src/pages/rttweb/[id]/files.tsx
@@ -1,31 +1,43 @@
-import axios from "axios"
-import { NextPage } from "next"
-import path from "path";
-import { useCallback } from "react"
+import axios from 'axios'
+import { NextPage } from 'next'
+import { useRouter } from 'next/router'
+import path from 'path'
+import { useCallback } from 'react'
 import { useDropzone } from 'react-dropzone'
 
-const FILES_API_FILE_FORM_KEY = 'file';
+const FILES_API_FILE_FORM_KEY = 'file'
 
 const Files: NextPage = () => {
+  const router = useRouter()
+  const { id } = router.query
+
   const onDrop = useCallback(async (acceptedFiles: File[]) => {
-    const formData = new FormData();
+    const formData = new FormData()
     acceptedFiles.map((file: File) => {
       console.log(file)
       formData.append(FILES_API_FILE_FORM_KEY, file)
     })
     try {
       console.log(formData.get('file'))
-      await axios.post('/api/genbas/1/files', formData)
+      await axios.post('/api/genbas/' + id + '/files', formData)
     } catch (e) {
       console.error(e)
     }
   }, [])
   const { getRootProps, getInputProps, isDragActive } = useDropzone({ onDrop })
 
-  return (<section className='container'><div {...getRootProps()}>
-    <input {...getInputProps({ className: 'dropzone' })} />
-    {isDragActive ? <p>Drop file here!</p> : <p>Drag 'n' drop some files here, or click to select files</p>}
-  </div></section>)
+  return (
+    <section className='container'>
+      <div {...getRootProps()}>
+        <input {...getInputProps({ className: 'dropzone' })} />
+        {isDragActive ? (
+          <p>Drop file here!</p>
+        ) : (
+          <p>Drag 'n' drop some files here, or click to select files</p>
+        )}
+      </div>
+    </section>
+  )
 }
 
 export default Files
